304e6d99b652fe62fea2e37cae33e8e4ec3f9537
YJKimSleepyWorker <dydwns5236@gmail.com>
2022-04-23 14:49:23 +0900

    브런치 연습 2

--- flutter_application_darttest1/lib/main.dart
+++ flutter_application_darttest1/lib/main.dart
@@ -102,7 +102,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // how it positions its children. Here we use mainAxisAlignment to
           // center the children vertically; the main axis here is the vertical
           // axis because Columns are vertical (the cross axis would be
-          // horizontal)222223333444444.
+          // horizontal)2222233334444445555555.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
@@ -126,7 +126,7 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
           FloatingActionButton(
           onPressed: _incrementCounter,
-          tooltip: 'Increment',
+          tooltip: '증가',
           child: const Icon(Icons.add),
         ), // This trailing comma makes auto-formatting nicer for build methods.
 
